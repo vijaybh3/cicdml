a51f0b1586810efbee15dd408188299601422b0b
vijaybh3 <vijay.bh3@gmail.com>
2025-11-06 11:30:55 +0530

    Update train_model.py
    Updated iris path

--- train_model.py
+++ train_model.py
@@ -11,7 +11,7 @@ from sklearn.metrics import confusion_matrix, precision_score, f1_score, recall_
 sns.set(style='white')
 
 # Load Data
-dataset = pd.read_csv(r'C:\Users\Admin\AVSCODE\CICD\code-2\iris.csv')
+dataset = pd.read_csv(r'iris.csv')
 
 # Feature names (Ensure no extra spaces or parentheses)
 dataset.columns = [colname.strip(' (cm)').replace(" ", "_") for colname in dataset.columns.tolist()]
@@ -135,3 +135,4 @@ with open('scores.txt', "w") as score:
     score.write("F1 Score: %2.1f%%\n" % f1_lr)
     score.write("Recall Score: %2.1f%%\n" % recall_lr)
     score.write("Precision Score: %2.1f%%\n" % prec_lr)
+
